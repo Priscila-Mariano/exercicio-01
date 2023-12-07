830f12f3d42e26ab45013599218290de0362ac13
Priscila-Mariano <priscila_mariano@hotmail.com.br>
2023-12-07 19:47:24 -0300

    primeiro commit

---
+++ desafio.js
@@ -0,0 +1,8 @@
+var filmeSerie = prompt("Você prefere filme ou série?");
+var nomeFilmeSerie = prompt("Qual você gosta mais?");
+
+let resultado = filmeSerie + ", você conhece " + nomeFilmeSerie;
+
+alert("Eu gosto de " + resultado + "?");
+
+
